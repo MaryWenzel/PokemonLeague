52408c7437b01a9b7fd7448f53376ee38f0b0777
alexanderalban <albanalexander@gmail.com>
2020-11-17 23:43:19 -0600

    battle functionality works so much better now! still have more conditions to set when I'm rested

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ let arrayOfPokemon;
 let pokeInfoArray1;
 let pokeInfoArray2;
 
+//Where Pokemon Type Arrays are populated
+
+let poke1Type1Array;
+let poke1Type2Array;
+let poke2Type1Array;
+let poke2Type2Array;
+
 //To Load Pokemon List
 window.onload = function () {
     getPokemon();
@@ -51,6 +58,8 @@ const generatePokemon = () => {
         li.appendChild(name)
         li.appendChild(ul)
         li.appendChild(type1)
+        // const type2 = document.createTextNode(`${capitalize(pokeInfoArray1.types[1]["type"]["name"])}`)
+
         firstPokemon.append(li)
     }, 1000)
 };
@@ -73,16 +82,77 @@ const generateSecondPokemon = () => {
         const img = document.createElement('img');
         const ul = document.createElement('ul');
         const name = document.createTextNode(` ${capitalize(pokeInfoArray2.name)}`)
+        const type1 = document.createTextNode(`${capitalize(pokeInfoArray2.types[0]["type"]["name"])}`)
         // img.src = pokeInfoArray1.sprites.front_default
         img.src = pokeInfoArray2["sprites"]["other"]['official-artwork']["front_default"];
         secondPokemon.appendChild(img)
         li.appendChild(name)
         li.appendChild(ul)
+        li.appendChild(type1)
         secondPokemon.append(li)
     }, 1000)
 };
 
 //Battle Button!
-let pokemon1 = pokeInfoArray1;
-let pokemon2 = pokeInfoArray2;
+const pokemonBattle = () => {
+    //Fetch Type 1 of Pokemon 1
+    fetch (pokeInfoArray1.types[0].type.url)
+    .then(response => response.json())
+    .then(pokemon => poke1Type1Array = pokemon)
+    //Fetch Type 1 of Pokemon 2
+    fetch (pokeInfoArray2.types[0].type.url)
+    .then(response => response.json())
+    .then(pokemon => poke2Type1Array = pokemon)
+    //Log Damage Relationships
+    setTimeout(function() {
+        console.log(poke1Type1Array['damage_relations'])
+        console.log(poke2Type1Array['damage_relations'])
+    }, 1000)
+
+    //Double Damage Determiner
+    const doubleDamage = () => {
+        //Move values into an array for far easier comparison
+        let superArray = poke1Type1Array['damage_relations']['double_damage_to'].map(({ name }) => name);
+        console.log(superArray);
+
+        if (poke1Type1Array['damage_relations']['double_damage_to'].length === 0) {
+            console.log("Not Super Effective");
+            console.log("theFirstOne");
+            return false;
+        } else {
+                if (superArray.includes(poke2Type1Array['name'])) {
+                    console.log('Super Effective!');
+                    return true;
+                } else {
+                    console.log('ELse');
+                    console.log("Not Super Effective");
+                    return false;
+                }
+        }
+    }
+    setTimeout(function() {
+        const firstPlayer = document.getElementById('first-result');
+        const secondPlayer = document.getElementById('second-result');
+        const banner = document.createElement('H1');
+        const bannerTwo = document.createElement('H1');
+        const winner = document.createTextNode(` Winner! `);
+        const loser = document.createTextNode(` Loser.`);
+        doubleDamage();
+        if (doubleDamage() === true) {
+            banner.appendChild(winner)
+            firstPlayer.append(banner);
+
+            bannerTwo.appendChild(loser)
+            secondPlayer.append(bannerTwo);
+        } else {
+            bannerTwo.appendChild(winner)
+            secondPlayer.append(bannerTwo);
+
+            banner.appendChild(loser)
+            firstPlayer.append(banner);
+        }
+    }, 1000)
+
+}
+
 
